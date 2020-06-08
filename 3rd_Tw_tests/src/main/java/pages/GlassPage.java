3db37d00b20f1e225f6e542acceba7bc97154598
nia <zsofi.deak.nia@gmail.com>
2020-06-08 14:44:36 +0200

    Finish POM files

---
+++ 3rd_Tw_tests/src/main/java/pages/GlassPage.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class GlassPage {
+    private WebDriver driver;
+    private By glassPermissionsTab = By.xpath("//a[contains(text(),'Permissions')]");
+    private By browseProjectGlassPermission = By.xpath("//div[@id='glass-permissions-panel']//tr[5]//td[3]//div[1]");
+    private By createIssueGlassPermission = By.xpath("//tr[8]//td[3]//div[1]");
+    private By editIssueGlassPermission = By.xpath("//tr[18]//td[3]//div[1]");
+
+
+    public GlassPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void navigateToGlassPermissions(){
+        driver.findElement(glassPermissionsTab).click();
+    }
+
+    public String getGlassPermissionForBrowseProject(){
+        if (driver.findElement(browseProjectGlassPermission).getAttribute("class").equals("glass-true-icon")){
+            return "Any logged in user";
+        }
+        return "Administrators";
+    }
+
+    public String getGlassPermissionForCreateIssue(){
+        if (driver.findElement(createIssueGlassPermission).getAttribute("class").equals("glass-true-icon")){
+            return "Any logged in user";
+        }
+        return "Administrators";
+    }
+
+    public String getGlassPermissionForEditIssue(){
+        if (driver.findElement(editIssueGlassPermission).getAttribute("class").equals("glass-true-icon")){
+            return "Any logged in user";
+        }
+        return "Administrators";
+    }
+}
--- 3rd_Tw_tests/src/main/java/pages/WelcomePage.java
+++ 3rd_Tw_tests/src/main/java/pages/WelcomePage.java
@@ -8,6 +8,8 @@ public class WelcomePage {
     private WebDriver driver;
     private By profilePic = By.xpath("//span[@class='aui-avatar aui-avatar-small']");
     private By logoutBtn = By.id("log_out");
+    private By issuesDrpd = By.id("find_link");
+    private By searchForIssue = By.id("issues_new_search_link_lnk");
 
     public WelcomePage(WebDriver driver) {
         this.driver = driver;
@@ -16,5 +18,10 @@ public class WelcomePage {
     public void logOutOfJira(){
         driver.findElement(profilePic).click();
         driver.findElement(logoutBtn).click();
+    }
+
+    public void navigateToIssuePage(){
+        driver.findElement(issuesDrpd).click();
+        driver.findElement(searchForIssue).click();
     }
 }
