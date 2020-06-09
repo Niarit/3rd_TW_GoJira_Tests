7b3915af4084e80e8bc731653fae9a3b8287dd4f
Gabor-Jalsovszky <gabor.jalsovszky@gmail.com>
2020-06-09 13:32:15 +0200

    Add browseAllProjects()

--- 3rd_Tw_tests/src/main/java/pages/AllProjectsPage.java
+++ 3rd_Tw_tests/src/main/java/pages/AllProjectsPage.java
@@ -7,9 +7,16 @@ import org.openqa.selenium.WebDriver;
 public class AllProjectsPage {
     private final WebDriver driver;
     private final By projects = By.xpath("//*[@id='browse_link']");
+    private final By allProjects = By.id("project_view_all_link_lnk");
     private final By mainTestingProject = By.linkText("Main Testing Project");
 
     public AllProjectsPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void navigateToAllProjects() {
+        driver.findElement(projects).click();
+        driver.findElement(allProjects).click();
+        driver.findElement(mainTestingProject).click();
     }
 }
---
+++ 3rd_Tw_tests/src/main/java/tests/BrowseProjectTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.AllProjectsPage;
+import pages.LoginPage;
+import pages.TestIssuePage;
+
+import java.util.concurrent.TimeUnit;
+
+public class BrowseProjectTest {
+
+    private WebDriver driver;
+    private final String driverPath = System.getenv("DRIVER_PATH");
+    private final String driverName = System.getenv("DRIVER");
+    private final String browserName = System.getenv("BROWSER");
+
+    @BeforeEach
+    public void setup(){
+        System.setProperty(driverName, driverPath);
+        if (browserName.equals("Firefox")){
+            driver = new FirefoxDriver();
+        } else if(browserName.equals("Chrome")){
+            driver = new ChromeDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+        LoginPage loginpage = new LoginPage(driver);
+        loginpage.logIntoJira(System.getenv("USER_NAME"),System.getenv("PW"));
+    }
+
+    @Test
+    public void browseAllProjects() {
+        AllProjectsPage allProjectsPage = new AllProjectsPage(driver);
+        allProjectsPage.navigateToAllProjects();
+    }
+}
