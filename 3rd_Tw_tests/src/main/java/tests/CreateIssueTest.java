d322efba5052d20dd5fe4a4c42315c86ab48fa0a
Gabor-Jalsovszky <gabor.jalsovszky@gmail.com>
2020-06-10 12:55:02 +0200

    Setup CreateIssueTest

---
+++ 3rd_Tw_tests/src/main/java/tests/CreateIssueTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.AllProjectsPage;
+import pages.LoginPage;
+import pages.ProjectPage;
+
+public class CreateIssueTest {
+
+    private final BaseTest baseTest = new BaseTest();
+
+    @BeforeEach
+    public void setup(){
+        baseTest.setup();
+        baseTest.loginToJira();
+    }
+
+    @AfterEach
+    public void close() {
+        baseTest.close();
+    }
+}
